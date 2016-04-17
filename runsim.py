eee8da35c343baa92b825455afe6e57b32c0b71b
John Kulp <John Kulp>
2016-04-16 22:46:40 -0400

    updates to students and moves alec's code to runsim

--- runsim.py
+++ runsim.py
@@ -1,10 +1,15 @@
-import course
-import faculty
-import record
 import student
+import record
+import faculty
+import course
 
+
+import random
+import scipy.stats as st
 import sys
 
+
+PASSFAIL = True
 
 def generate_core_courses(num_core):
 	courses = []
@@ -22,6 +27,20 @@ def generate_students(num_to_generate):
 	for i in range(num_to_generate):
 		students.append(Student(skill_level = random.random()))
 
+
+# a term is a list of courses
+def term_occur(term):
+	for course in term:
+		for student in course.students:
+			grade = grading(course.difficulty, PASSFAIL)
+			student.add_course(course.classID,grade)
+
+def grading(difficulty, passfail = True):
+	if passfail:
+		return 1.0 if random.random() >= (1-difficulty) else 0.0
+	else:
+		grade = random.random()
+		return grade if grade >= () (1-difficulty) else 0.0
 
 
 def runloop(students, term, num_incoming):
