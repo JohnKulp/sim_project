2531cd749be5b523a207199235a18c3edd808dfc
John Kulp <John Kulp>
2016-04-16 22:06:04 -0400

    adds a generate courses function

--- runsim.py
+++ runsim.py
@@ -6,10 +6,19 @@ import student
 import sys
 
 
+def generate_courses(num_core):
+	courses = []
+
+	for i in range(num_core):
+		courses.append(Course(i, class_size = 30))
+
+	return courses
+
+
 def runloop():
 	pass
 
-	
+
 
 if __name__ == "__main__":
 
@@ -17,6 +26,9 @@ if __name__ == "__main__":
 		print("run with number of iterations as the only argument")
 		
 	else:
+
+		courses = generate_courses(20)
+
 		num_iterations = int(sys.argv[1])
 
 		for i in range(num_iterations):
