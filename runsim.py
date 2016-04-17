f618e44f3523481e02447c8b836112df30302184
John Kulp <John Kulp>
2016-04-16 23:44:47 -0400

    housekeeping

--- runsim.py
+++ runsim.py
@@ -1,9 +1,9 @@
-import student
-import record
-import faculty
-import course
+import Student
+import Record
+import Faculty
+import Course
 
-
+import operator
 import random
 import scipy.stats as st
 import sys
@@ -43,6 +43,28 @@ def update_students_with_new_grades(term):
 		for student in course.students:
 			grade = grading(course.difficulty, PASSFAIL)
 			student.add_course(course.classID,grade)
+
+
+def populate_courses(term,students):
+	students.sort(key=operator.attrgetter(students.credits), reverse=True)
+	for student in students:
+		courses_taken=[]
+		for courseID in student.plan_to_retake:
+			for i in range(len(term)):
+				if courseID == term[i].classID:
+					if(term[i].class_size>len(term[i].students)):
+						courses_taken.append(courseID)
+						term[i].students.append(student)
+						student.plan_to_retake.remove(courseID)
+						if courses_taken == 3:
+							break					
+		random.shuffle(term)
+		for course in term:
+			if course.courseID not in courses_taken and course.courseID not in student.course_transcript and course.class_size>len(course.students):					
+				courses_taken.append(courseID)
+				course.students.append(student)
+				if courses_taken == 3:
+					break
 
 
 def find_leaving_students(students):
