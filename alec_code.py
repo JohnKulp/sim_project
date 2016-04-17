d8d6b257e17012b30497b5d88bf1d7c4e3603fa9
Alec Jasen <acjn11@gmail.com>
2016-04-16 23:07:27 -0400

    added function to update plans to retake courses the next semester

--- alec_code.py
+++ alec_code.py
@@ -3,20 +3,12 @@ import record
 import faculty
 import course
 import random
-import scipy.stats as st
 
-PASSFAIL = True
 
-# a term is a list of courses
-def term_occur(term):
+# term is a list of courses for the semester
+def find_plans_to_retake(term):
 	for course in term:
 		for student in course.students:
-			grade = grading(course.difficulty, PASSFAIL)
-			student.add_course(course.classID,grade)
-
-def grading(difficulty, passfail = True):
-	if passfail:
-		return 1.0 if random.random() >= (1-difficulty) else 0.0
-	else:
-		grade = random.random()
-		return grade if grade >= () (1-difficulty) else 0.0
+			# this key should exist by the time this is called
+			if student.course_transcript[course.classID] == 0:
+				student.plan_to_retake.append(course.classID)
