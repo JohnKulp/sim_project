34b757e5d7789c4865ffb0382510721e96f8819d
John Kulp <John Kulp>
2016-04-20 16:33:33 -0400

    I think this actually removes them

--- Faculty.py
+++
@@ -1,9 +0,0 @@
-
-class Faculty():
-
-	def __init__(self, ID=None, courses=[], difficulty=0.0):
-		self.ID = ID
-		self.courses = courses		# a list of course objects
-		self.difficulty = difficulty
-
-
