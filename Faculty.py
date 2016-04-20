ca422dae6efb2966b02f3c87dce505f339a47f38
John Kulp <John Kulp>
2016-04-20 16:38:45 -0400

    does this fix it?

---
+++ Faculty.py
@@ -0,0 +1,9 @@
+
+class Faculty():
+
+	def __init__(self, ID=None, courses=[], difficulty=0.0):
+		self.ID = ID
+		self.courses = courses		# a list of course objects
+		self.difficulty = difficulty
+
+
