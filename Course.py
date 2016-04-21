3a84358a4df1133918a7a1e2fd25e5ad066f74ce
Alec Jasen <acjn11@gmail.com>
2016-04-20 23:32:09 -0400

    cleaned up course again

--- Course.py
+++ Course.py
@@ -5,12 +5,11 @@ import random
 class Course():
 
 	#term is 1 for odd, 2 for even, 3 for all
-	def __init__(self, course_id, is_core =True, class_size=float('inf'), students=None, waitlist = None, difficulty=0, term=3, requirements = None):
+	def __init__(self, course_id, is_core =True, class_size=float('inf'), students=None, difficulty=0, term=3, requirements = None):
 		self.course_id = course_id
 		self.class_size = class_size
 		self.students = [] if students == None else students
-		self.waitlist = [] if students == None else students
-		self.students = students			#students is a list of Student IDs
+		self.waitlist = []
 		self.difficulty = difficulty
 		self.term = term
 		self.is_core = is_core
