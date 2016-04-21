c3d08389b6c809bd4863faf1b2f0c78894461f17
Alec Jasen <acjn11@gmail.com>
2016-04-20 23:30:52 -0400

    cleaned up Course object

--- Course.py
+++ Course.py
@@ -8,12 +8,12 @@ class Course():
 	def __init__(self, course_id, is_core =True, class_size=float('inf'), students=None, waitlist = None, difficulty=0, term=3, requirements = None):
 		self.course_id = course_id
 		self.class_size = class_size
-		students = [] if students == None else students
-		waitlist = [] if students == None else students
+		self.students = [] if students == None else students
+		self.waitlist = [] if students == None else students
 		self.students = students			#students is a list of Student IDs
 		self.difficulty = difficulty
 		self.term = term
 		self.is_core = is_core
-		requirements = [] if requirements == None else requirements
+		self.requirements = [] if requirements == None else requirements
 		self.requirements = requirements
 
