7281548f4d87a44e9eda4d1b8165f446a32066b0
sroomberg <stevenroomberg@gmail.com>
2016-04-25 19:31:29 -0400

    More steady state results

--- steven_code.py
+++ steven_code.py
@@ -2,9 +2,9 @@ from runsim import *
 
 if __name__ == "__main__":
     steady_state_file = open("steady_state_res.txt", "w")
-    steady_state_file.write("Iteration GradRate AvgGradTime")
-    for i in range(1,1000):
-        grad_stats = main_loop(i, 10, 0)
-        steady_state_file.write("%d %.2f %.2f" % (i, grad_stats[0], grad_stats[1]))
+    steady_state_file.write("Iteration GradRate AvgGradTime\n")
+    for i in range(118, 200):
+        grad_stats = main_loop(i, 1, 0)
+        steady_state_file.write("%d %.2f %.2f\n" % (i, grad_stats[0], grad_stats[1]))
 
     steady_state_file.close()
